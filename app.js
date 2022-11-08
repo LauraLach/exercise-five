0106d5ab14365103b47e62413b66754b87379279
Laura Lachin <lauralachin@10-18-40-223.dynapool.wireless.nyu.edu>
2022-11-08 09:18:32 -0500

    Added firebase

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
 const express = require('express');
 // const router = express.Router();
+const firebase = require("firebase/app");
 const app = express();
 const port = process.env.PORT || 4000;
 
+const firebaseConfig = {
+    apiKey: "AIzaSyBPNfTEMJvq3lojLBdNYHxVn6qXvoOL5m8",
+    authDomain: "exercise-five-11d34.firebaseapp.com",
+    projectId: "exercise-five-11d34",
+    storageBucket: "exercise-five-11d34.appspot.com",
+    messagingSenderId: "382140343079",
+    appId: "1:382140343079:web:28ca16ed76be9195ffbf02",
+    measurementId: "G-0SMCZEN7ZV"
+  };
+firebase.initializeApp(firebaseConfig);
+  
+
 const indexRoute = require('./routes/index');
+const singlePostRoute = require('./routes/singlePost');
 
 app.use('/', indexRoute);
+app.use('/post', singlePostRoute);
 
 app.listen(port, () => console.log("Working!"));
---
+++ routes/singlePost.js
@@ -0,0 +1,28 @@
+const { response } = require('express');
+const express = require('express')
+const router = express.Router()
+const firestore = require("firebase/firestore");
+const db = firestore.getFirestore();
+
+router.use((req, res, next) => {
+  console.log('Time: ', Date.now());
+  next();
+})
+
+router.get('/:id', (req, res) => {
+    const postID = req.params.id;
+    const postQuery = firestore.getDoc(firestore.doc(db, "posts", postID));
+
+  postQuery
+    .then((response) => {
+      const post = response.data();
+      if(!post) res.send({});
+      res.send(post);
+    })
+    .catch((error) => {
+      console.log(error);
+      return res.send(error);
+    });
+  });
+
+module.exports = router;
